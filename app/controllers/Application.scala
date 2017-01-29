561fc883e7320bad6467e4880d3d8ca1e146fb0e
Andrii Kondiuk <andriikondiuk@gmail.com>
2017-01-29 20:56:33 +0000

    reactive plumbing for the data from Twitter

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,16 @@
 package controllers
 
 //import com.ning.http.client.oauth.{ConsumerKey, RequestToken}
-import actors.TwitterStreamer
 import play.api.Play.current
-import play.api._
-import play.api.libs.iteratee.Iteratee
-import play.api.libs.json.JsValue
+import play.api.libs.iteratee.{Concurrent, Enumeratee, Enumerator, Iteratee}
+import play.api.libs.json._
 import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
 import play.api.libs.ws.WS
 import play.api.mvc._
-import play.api.Logger
-import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.{Logger, _}
+import play.extras.iteratees._
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 
@@ -49,18 +48,28 @@ class Application extends Controller {
 
   def tweets = Action.async {
 
-    val loggingIteratee = Iteratee.foreach[Array[Byte]] { array =>
-      Logger.info(array.map(_.toChar).mkString)
-    }
-
     credentials.map { case (consumerKey, requestToken) =>
+
+      val (iteratee, enumerator) = Concurrent.joined[Array[Byte]]
+
+      val jsonStream: Enumerator[JsObject] =
+        enumerator &>
+          Encoding.decode() &>
+          Enumeratee.grouped(JsonIteratees.jsSimpleObject)
+
+      val loggingIteratee = Iteratee.foreach[JsObject] { value =>
+        Logger.info(value.toString)
+      }
+
+      jsonStream run loggingIteratee
+
       WS
         .url("https://stream.twitter.com/1.1/statuses/filter.json")
         .sign(OAuthCalculator(consumerKey, requestToken))
-        .withQueryString("track" -> "reactive")
+        .withQueryString("track" -> "brexit")
         .get { response =>
           Logger.info("Status: " + response.status)
-          loggingIteratee
+          iteratee
         }.map { _ =>
         Ok("Stream closed")
       }
